c8acb9cc43c3b4a6366adcd68e886affccebd675
AleksandreBegiashvili <Begiashvili99@gmail.com>
2020-02-11 13:14:14 +0400

    commit 11 feb

--- Autoniverse/Controllers/NotificationController.cs
+++ Autoniverse/Controllers/NotificationController.cs
@@ -14,14 +14,37 @@ namespace Autoniverse.Controllers
     {
         [HttpGet]
         [Produces("text/plain")]
-        public ContentResult GetEmailConfirmationPage()
+        public ContentResult GetEmailConfirmationPage(bool hasCredentials)
         {
-            return new ContentResult
+            if (hasCredentials)
             {
-                ContentType = "text/html",
-                StatusCode = (int)HttpStatusCode.OK,
-                Content = "<html><body>This is email confirmation page</body></html>"
-            };
+
+                string htmlContent = "<div class=\"card\">" +
+                    "<div  class=\"box\">" +
+                    " <div class=\"img\">" +
+                    "<!--<img src=\"~/ images / logo.png\">-->" +
+                    " </div>" +
+                    "<h2>Thank You<br><span>Email Confirmed</span></h2>" +
+                    "<p>" +
+                    " You can now successfuly login. You have full access to our products and services." +
+                    "</p>" +
+                    "<a class=\"btn btn-primary btn - block\" [routerLink]=\"['/login']\" >LOGIN</a>" +
+                    " </div>" +
+                    "</div>";
+                return new ContentResult
+                {
+                    ContentType = "text/html",
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Content = htmlContent
+                };
+            } 
+            else
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 500
+                };
+            }
         }
     }
 }
