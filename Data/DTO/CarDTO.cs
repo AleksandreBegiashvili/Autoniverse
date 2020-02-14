cf94dec75213c005bc80b6b191b7af759a2d29d6
Sandro Begiashvili <Begiashvili@gmail.com>
2020-02-14 17:08:51 +0400

    finished techhowdy course

--- Data/DTO/CarDTO.cs
+++ Data/DTO/CarDTO.cs
@@ -9,11 +9,11 @@ namespace Data.DTO
 {
     public class CarDTO
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
 
         public int CarCategoryId { get; set; }
-        [ForeignKey("CarCategoryId")]
-        public CarCategory CarCategory { get; set; }
+        //[ForeignKey("CarCategoryId")]
+        //public CarCategory CarCategory { get; set; }
 
         [Required]
         public string Make { get; set; }
@@ -21,8 +21,8 @@ namespace Data.DTO
         [Required]
         public string Model { get; set; }
 
-        [Required]
-        public string SaleType { get; set; }
+        //[Required]
+        //public string SaleType { get; set; }
 
         [Required]
         public int ProductionYear { get; set; }
@@ -30,60 +30,60 @@ namespace Data.DTO
         [Required]
         public int Price { get; set; }
 
-        [Required]
-        public string Transmission { get; set; }
+        //[Required]
+        //public string Transmission { get; set; }
 
-        [Required]
-        public string FuelType { get; set; }
+        //[Required]
+        //public string FuelType { get; set; }
 
-        [Required]
-        public int Odometer { get; set; }
+        //[Required]
+        //public int Odometer { get; set; }
 
-        [Required]
-        public int EngineCapacity { get; set; }
+        //[Required]
+        //public int EngineCapacity { get; set; }
 
-        [Required]
-        public string Color { get; set; }
+        //[Required]
+        //public string Color { get; set; }
 
-        [Required]
-        public int NumberOfCylinders { get; set; }
+        //[Required]
+        //public int NumberOfCylinders { get; set; }
 
-        [Required]
-        public int NumberOfDoors { get; set; }
+        //[Required]
+        //public int NumberOfDoors { get; set; }
 
-        [Required]
-        public string WheelDriveType { get; set; }
+        //[Required]
+        //public string WheelDriveType { get; set; }
 
-        [Required]
-        public string InteriorColor { get; set; }
+        //[Required]
+        //public string InteriorColor { get; set; }
 
-        [Required]
-        public int NumberOfAirbags { get; set; }
+        //[Required]
+        //public int NumberOfAirbags { get; set; }
 
-        [Required]
-        public string SteeringWheelPosition { get; set; }
+        //[Required]
+        //public string SteeringWheelPosition { get; set; }
 
         [Required]
         public string Description { get; set; }
 
-        [Required]
-        public string SellerName { get; set; }
+        //[Required]
+        //public string SellerName { get; set; }
 
-        [Required]
-        public int SellerNumber { get; set; }
+        //[Required]
+        //public int SellerNumber { get; set; }
 
-        // Boolean properties
-        public bool IsValidated { get; set; }
-        public bool HasABS { get; set; }
-        public bool HasTCS { get; set; }
-        public bool HasAlarm { get; set; }
-        public bool HasElectricMirrors { get; set; }
-        public bool HasLeatherInterior { get; set; }
-        public bool HasClimateControl { get; set; }
-        public bool HasHesatedSeats { get; set; }
-        public bool HasNavigation { get; set; }
-        public bool HasAirConditioning { get; set; }
-        public bool HasParkingControl { get; set; }
-        public bool HasRearCamera { get; set; }
+        //// Boolean properties
+        //public bool IsValidated { get; set; }
+        //public bool HasABS { get; set; }
+        //public bool HasTCS { get; set; }
+        //public bool HasAlarm { get; set; }
+        //public bool HasElectricMirrors { get; set; }
+        //public bool HasLeatherInterior { get; set; }
+        //public bool HasClimateControl { get; set; }
+        //public bool HasHesatedSeats { get; set; }
+        //public bool HasNavigation { get; set; }
+        //public bool HasAirConditioning { get; set; }
+        //public bool HasParkingControl { get; set; }
+        //public bool HasRearCamera { get; set; }
     }
 }
